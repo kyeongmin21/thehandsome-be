6d517186a1495cb29da159ef41716908fdad93d6
kyeongmin <kyeongmin@nglife.co.kr>
2025-09-10 17:43:58 +0900

    api router 정의

--- app/api/boards.py
+++ app/api/boards.py
@@ -8,20 +8,30 @@ router = APIRouter()
 boards = [
     {"id": 1, "title": "첫 글", "content": "게시판 글입니다.", "created_at": '', "x": ''},
     {"id": 2, "title": "공지사항", "content": "여기는 공지입니다.", "created_at": '', "updated_at": ''},
+    {"id": 3, "title": "공지사항", "content": "여기는 공지입니다.", "created_at": '', "updated_at": ''},
 ]
 
 # GET
-@router.get("/")
+@router.get("")
 def get_boards():
     return boards
 
 
+# GET: 단일 글 조회
+@router.get("/{board_id}")
+def get_board(board_id: int):
+    for board in boards:
+        if board["id"] == board_id:
+            return board
+    raise HTTPException(status_code=404, detail="글을 찾을 수 없습니다.")
+
+
+# POST: 글 작성
 class BoardCreate(BaseModel):
     title: str
     content: str
 
-# POST: 글 작성
-@router.post("/")
+@router.post("/create")
 def create_boards(board: BoardCreate):
     new_id = max(post["id"] for post in boards) + 1 if boards else 1
     now = datetime.now()
@@ -37,20 +47,26 @@ def create_boards(board: BoardCreate):
 
 
 
+
 # UPDATE
-@router.put("/boards/{board_id}")
-def update_boards(board_id: int, title: str, content: str):
+class BoardUpdate(BaseModel):
+    title: str
+    content: str
+
+@router.put("/edit/{board_id}")
+def update_boards(board_id: int, update: BoardUpdate):
+    now = datetime.now()
     for board in boards:
         if board["id"] == board_id:
-            board["title"] = title
-            board["content"] = content
-            board["updated_at"] = datetime.now().isoformat()  # 수정 시간 갱신
+            board["title"] = update.title
+            board["content"] = update.content
+            board["updated_at"] = now.strftime("%Y-%m-%d %H:%M")
             return board
     raise HTTPException(status_code=404, detail="글을 찾을 수 없습니다.")
 
 
 # DELETE: 삭제
-@router.delete("/boards/{board_id}")
+@router.delete("/{board_id}")
 def delete_board(board_id: int):
     for board in boards:
         if board["id"] == board_id:
