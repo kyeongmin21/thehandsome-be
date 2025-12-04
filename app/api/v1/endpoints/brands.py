ddc3e6358a259f5a61bb6d26a15786fc1b60ef49
kim kyeong min <kyeongmin21@naver.com>
2025-12-04 22:02:24 +0900

    브랜드 코드 수정

--- app/api/v1/endpoints/brands.py
+++ app/api/v1/endpoints/brands.py
@@ -1,7 +1,8 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 
+from app.crud.crud_brandlike import get_user_brand_wishes
 from app.crud.crud_brands import get_brands
 from app.database import get_db
 from app.schemas.brands import BrandGroup
@@ -11,24 +12,26 @@ router = APIRouter()
 
 # 전체 브랜드 조회
 @router.get("/list", response_model=List[BrandGroup], summary="전체 브랜드 조회")
-def get_all_brands(db: Session = Depends(get_db)):
+def get_all_brands(request: Request, db: Session = Depends(get_db)):
     brands = get_brands(db)
+
+    # 로그인 되어 있으면 찜 정보 가져오기
+    user = getattr(request.state, "user", None)
+    wishlist_codes = []
+    if user:
+        wishlist = get_user_brand_wishes(db, user["id"])
+        wishlist_codes = [bl.brand_code for bl in wishlist]
 
     grouped = {}
 
     for b in brands:
-        if b.brand_type not in grouped:
-            grouped[b.brand_type] = []
-
-        grouped[b.brand_type].append({
+        grouped.setdefault(b.brand_type, []).append({
             "brand_code": b.brand_code,
-            "brand_name": b.brand_name
+            "brand_name": b.brand_name,
+            "is_wishlist": b.brand_code in wishlist_codes
         })
 
-    # JSON 형태로 변환
-    result = [
-        {"brand_type": brand_type, "brands": brand_list}
-        for brand_type, brand_list in grouped.items()
+    return [
+        {"brand_type": k, "brands": v}
+        for k, v in grouped.items()
     ]
-
-    return result
